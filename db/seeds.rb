045bc15e5f54e144175c59ad300f5c297f9e0d26
Meric BAL <MB@Meric-MacBook-Air.local>
2017-02-20 15:18:06 -0800

    likes added.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,33 @@
+User.destroy_all
+Tweet.destroy_all
+Like.destroy_all
+#
+puts 'Destroyed all tables !'
+#
+
 User.create(username: 'a', email: 'a', password: 'aaa')
 User.create(username: 'b', email: 'b', password: 'bbb')
 User.create(username: 'c', email: 'c', password: 'ccc')
 
 
-10.times do
+2.times do
 	Tweet.create(body: Faker::Lorem.sentence, user_id: 1)
 end
-10.times do
+2.times do
 	Tweet.create(body: Faker::Lorem.sentence, user_id: 2)
 end
-10.times do
+2.times do
 	Tweet.create(body: Faker::Lorem.sentence, user_id: 3)
 end
+
+4.times do |x|
+	Like.create(user_id: x, tweet_id: 1)
+	Like.create(user_id: x, tweet_id: 3)
+	Like.create(user_id: x, tweet_id: 5)
+end
+
+
+
 
 #
 puts 'Seed complete !'
