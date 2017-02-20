9878d2f5c9d74ea63b67a7fc74f29f88d953a3a3
Meric BAL <MB@Meric-MacBook-Air.local>
2017-02-19 20:37:43 -0800

    user controller.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,12 +5,24 @@ class UsersController < ApplicationController
 	end
 
 	def create
+		@user = User.new(user_params)
+		if @user.save
+			login(@user)
+			redirect_to '/'
+			# redirect_to user_path(@user)
+		else
+			errors = @user.errors.full_messages
+			render :new
+		end
 	end
 
+	def show
+		@user = User.find(params[:id])
+	end
 
 	private
 
 	def user_params
-		# params.require(:user).permit(:username, :email, :password)
+		params.require(:user).permit(:username, :email, :password)
 	end
 end
