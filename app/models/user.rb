34f3d1940252731d0181911cfd8eaac6017a6328
Meric BAL <MB@Meric-MacBook-Air.local>
2017-02-20 00:12:37 -0800

    validations.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
 	has_secure_password
+	has_mant :tweets
+	
 	validates :username, :email, presence: true
 	validates :username, :email, uniqueness: true
 	validates :password, length: { minimum: 3, too_short: ' has to be more than %{count} characters. ' }
