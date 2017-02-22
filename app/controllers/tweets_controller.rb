52d1fb9cb051b9318f1d3a53f37d648283398735
Meric BAL <MB@Meric-MacBook-Air.local>
2017-02-21 22:20:45 -0800

    deleting tweets.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,11 +1,9 @@
 class TweetsController < ApplicationController
-	before_action :set_user, only: [:new, :create]
+	before_action :set_user, only: [:new, :create, :destroy]
 
-	# include SessionsHelper
-
-	def index
-		# @tweets = @user.tweets
-	end
+	# def index
+	# 	@tweets = @user.tweets
+	# end
 
 	def new
 		@tweet = Tweet.new()
@@ -16,10 +14,17 @@ class TweetsController < ApplicationController
 		@tweet.user_id = @user.id
 		if @tweet.save
 			@user.tweets << @tweet
+			# redirect_to "/users/#{@user.id}#tweets"
 			redirect_to user_path(@user)
 		else
 			render :new
 		end
+	end
+
+	def destroy
+		@tweet = Tweet.find(params[:id])
+		@tweet.destroy
+		redirect_to user_path(@user)
 	end
 
 	private
@@ -31,4 +36,5 @@ class TweetsController < ApplicationController
 	def tweet_params
 		params.require(:tweet).permit(:body)
 	end
+
 end
