fdd4f238453637cdd731839015b60ee46d3fd029
Meric BAL <MB@Meric-MacBook-Air.local>
2017-02-24 17:33:00 -0800

    Schema update.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170223012911) do
+ActiveRecord::Schema.define(version: 20170225012332) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,13 @@ ActiveRecord::Schema.define(version: 20170223012911) do
   create_table "likes", force: :cascade do |t|
     t.integer  "user_id"
     t.integer  "tweet_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tweet_hashtags", force: :cascade do |t|
+    t.integer  "tweet_id"
+    t.integer  "hashtag_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
