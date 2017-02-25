fdd4142724907394931d57c14defc812fcce39d5
Meric BAL <MB@Meric-MacBook-Air.local>
2017-02-24 17:27:19 -0800

    Tweet model updated.

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,9 +1,15 @@
 class Tweet < ApplicationRecord
 	belongs_to :user
+
+	#likes and dislikes
 	has_many :likes
 	has_many :liked_users, through: :likes, source: :user
 	has_many :dislikes
 	has_many :disliked_users, through: :dislikes, source: :user
+
+	#Hashtags
+	has_many :tweet_hashtags
+	has_many :hashtags, through: :tweet_hashtags
 
 	def since?
     ((Time.now - created_at)/3600).round # hours ago
