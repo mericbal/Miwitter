7c4854fd7bc86a013bdbde212742716cd8a57418
Meric BAL <MB@Meric-MacBook-Air.local>
2017-02-27 19:21:29 -0800

    Likes controller modified.

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,17 +1,24 @@
 class LikesController < ApplicationController
-
+	before_action :set_user, only: [:like, :dislike]
+	
 	include SessionsHelper
 
 	def like
 		@tweet = Tweet.find(params[:tweet_id])
 		@tweet.liked_users << current_user
-		redirect_to user_path(current_user)
+		redirect_to user_path(@user)
 	end
 
 	def dislike
 		@tweet = Tweet.find(params[:tweet_id])
 		@tweet.disliked_users << current_user
-		redirect_to user_path(current_user)
+		redirect_to user_path(@user)
+	end
+
+	private
+
+	def set_user
+		@user = User.find(params[:user_id])
 	end
 
 end
