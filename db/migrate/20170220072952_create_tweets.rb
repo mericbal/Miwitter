bcf4aaf865d31a44fc7d29bf82b568e1112e0240
Meric BAL <MB@Meric-MacBook-Air.local>
2017-03-05 00:48:49 -0800

    Private option added on tweets table .

--- db/migrate/20170220072952_create_tweets.rb
+++ db/migrate/20170220072952_create_tweets.rb
@@ -2,6 +2,7 @@ class CreateTweets < ActiveRecord::Migration[5.0]
   def change
     create_table :tweets do |t|
     	t.string :body
+    	t.boolean :private
     	t.references :user
 
     	t.timestamps
