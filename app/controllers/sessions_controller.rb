535287cee8690f4d726682eef7da5570e9b34fa9
Meric BAL <MB@Meric-MacBook-Air.local>
2017-03-06 15:58:54 -0800

    Sessoin controller modified.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
 	end
 
 	def create
-		@user = User.find_by(email: params[:email])
+		@user = User.find_by(email: params[:email]) || @user = User.find_by(username: params[:username])
 		if @user && @user.authenticate(params[:password])
 			login(@user)
 			redirect_to user_path(@user)
