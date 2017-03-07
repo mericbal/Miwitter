a0f847825740093b26d9b4d3f5b46a78a0d11281
Meric BAL <MB@Meric-MacBook-Air.local>
2017-03-06 19:34:03 -0800

    Improvements.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
 	include FollowsHelper
 
 	# paperclip
-	has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+	has_attached_file :avatar, styles: { large: "500x500", medium: "300x300>", thumb: "100x100>" }, default_url: "anon1.jpeg"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 
 	# tweets
