1a9cd4213cd6687e2daed9d60a544fa7329f664c
Meric BAL <MB@Meric-MacBook-Air.local>
2017-03-06 16:45:49 -0800

    Paperclip gem and user model updated.

--- Gemfile
+++ Gemfile
@@ -51,5 +51,7 @@ end
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
+# new gems
 gem 'awesome_print'
 gem 'faker'
+gem "paperclip", "~> 5.0.0"
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ApplicationRecord
 	has_secure_password
 	include FollowsHelper
-	
+
+	# paperclip
+	has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+
 	# tweets
 	has_many :tweets
 	has_many :likes
