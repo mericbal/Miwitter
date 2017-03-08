cb219fafcb38805897d3a9b49e073baa33fc7939
Meric BAL <MB@Meric-MacBook-Air.local>
2017-03-07 21:39:05 -0800

    Updates and improvements.

--- db/migrate/20170220024442_create_users.rb
+++ db/migrate/20170220024442_create_users.rb
@@ -1,7 +1,8 @@
 class CreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
-    	t.string :username, :email, :password_digest
+    	t.string :first, :last, :username, :email, :password_digest
+    	t.attachment :avatar
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170307004710) do
+ActiveRecord::Schema.define(version: 20170225012332) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20170307004710) do
 
   create_table "hashtags", force: :cascade do |t|
     t.string   "name"
+    t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -61,15 +62,17 @@ ActiveRecord::Schema.define(version: 20170307004710) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string   "first"
+    t.string   "last"
     t.string   "username"
     t.string   "email"
     t.string   "password_digest"
-    t.datetime "created_at",          null: false
-    t.datetime "updated_at",          null: false
     t.string   "avatar_file_name"
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
   end
 
 end
